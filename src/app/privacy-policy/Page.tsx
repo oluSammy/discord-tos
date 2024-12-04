fb724d38c1aec37bcf6104890b769246a721b1fe
olusammy <olumorinsammy@gmail.com>
2024-12-04 11:28:19 +0100

    terms of service

---
+++ src/app/privacy-policy/Page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>Last updated: [Insert Date]</p>
+      <h2>Information Collection And Use</h2>
+      <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
+      <h3>Use of Data</h3>
+      <p>The data we collect will be used to support your experience throughout this app, to manage access to your account, and for other purposes such as analytics and advertisements.</p>
+      <h3>Disclosure Of Data</h3>
+      <h3>Security Of Data</h3>
+      <h3>Changes To This Privacy Policy</h3>
+      <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+      <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/tos/Page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <h2>Welcome to Our Discord App!</h2>
+      <p>By accessing or using our Discord app, you agree to be bound by these terms of service. If you disagree with any part of the terms, you may not access the service.</p>
+      <h3>Usage License</h3>
+      <p>Permission is granted to temporarily download one copy of the materials on our app for personal, non-commercial transitory viewing only.</p>
+      <h3>Disclaimer</h3>
+      <p>All the materials on our app are provided &quot;as is&quot;. We make no warranties, may it be expressed or implied, therefore negates all other warranties.</p>
+      <h3>Limitations</h3>
+      <p>In no event shall our app or its suppliers be liable for any damages arising out of the use or inability to use the materials on our app.</p>
+      <h3>Governing Law</h3>
+      <p>Any claim related to our Discord app shall be governed by the laws of our state without regards to its conflict of law provisions.</p>
+    </div>
+  )
+}
+
+export default Page
